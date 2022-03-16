db8f65194fa5338f7e3852c8562c551b6c26dcd0
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-16 17:15:06 -0400

    Commentary on note taker functionality added

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
+// Import for Express package
 const express = require('express');
+// Import for fs package to write and read files
 const fs = require('fs');
+// Import for path package to link front-end with back-end
 const path = require('path');
+// Import for uuid package to generate unique IDS for new and previous notes
 const {v4 : uuidv4} = require('uuid');
 
-
+// Import for clog module
 const {clog} = require('./middleware/clog');
 
+// Local host port for Heroku upload
 const PORT = process.env.PORT || 3001;
 
+// Initializing app with express
 const app = express();
 
+// Statement for app to use custom middleware
 app.use(clog);
 
+// Middleware for hadling encrypted and JSON data using express
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+// Serving static files from the public folder
 app.use(express.static('public'));
 
 // GET route for homepage
@@ -27,14 +36,14 @@ app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
-// GET to access previous notes
+// GET handle to access previous notes
 app.get('/api/notes', (req, res) => {
     const prevNote = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf8");
     const parsedNote = JSON.parse(prevNote);
     res.json(parsedNote);
 });
 
-// POST to handle posting new notes
+// POST handle for posting new notes
 app.post('/api/notes', (req, res) => {
     const notes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf8");
     const parsedNote = JSON.parse(notes);
@@ -52,6 +61,7 @@ app.post('/api/notes', (req, res) => {
 
 });
 
+// Listener for app connection to the local port
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT} 🚀`);
 });
